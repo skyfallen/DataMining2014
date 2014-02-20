0a7e7f5931b232796f58316cafd4cc2b02d451aa
annitrolla <annitrolla@gmail.com>
2014-02-20 22:41:34 +0200

    5th and 6th tasks

---
+++ HW2/practice2.R
@@ -0,0 +1,35 @@
+#install necessary packages (run only once)
+install.packages("arules")
+install.packages("arulesViz")
+
+#load datafrom the url
+data_url = url("https://courses.cs.ut.ee/MTAT.03.183/2014_spring/uploads/Main/titanic.txt")
+titanic = read.table(data_url, sep =',', header = TRUE)
+
+#observe the data
+##first 6 observations
+head(titanic)
+#types of features
+str(titanic)
+#dimensionality of the data
+dim(titanic)
+
+#load package for frequent set mining
+library(arules)
+#help with apriori
+?apriori
+#run apriori algorithm with default settings
+rules = apriori(titanic)
+#inspection of the result
+inspect(rules)
+
+#now let us assume, we want to see only those rules that have rhs as survived:
+rules = apriori(titanic,appearance = list(rhs=c("Survived=No", "Survived=Yes"),default="lhs"))
+inspect(rules)
+
+#let us relax the default settings for the rules we are looking for
+rules = apriori(titanic,parameter = list(minlen=2, supp=0.05, conf=0.8),appearance = list(rhs=c("Survived=No", "Survived=Yes"),default="lhs"))
+
+#visualization
+library(arulesViz)
+plot(rules, method="graph", control=list(type="items"))
