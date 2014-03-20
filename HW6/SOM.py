c297ee6fe15245fd52025b87f9ed65e513714064
unknown <dmytrofishman@gmail.com>
2014-03-20 20:57:16 +0200

    'HW6'

---
+++ HW6/SOM.py
@@ -0,0 +1,16 @@
+'''
+Self-organizing map implementation
+Language: Python
+Data Mining
+University of Tartu
+'''
+def init():
+    print("init")
+
+def find_winner():
+    print('finding a winner')
+
+def update_nodes():
+    print('updating nodes')
+    
+update_nodes()
